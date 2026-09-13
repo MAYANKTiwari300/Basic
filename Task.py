81be8b0994c9ce208458593f22501ca3ecb48af5
MayankTiwari3000 <mayankchandratiwari@gmail.com>
2026-09-13 14:28:30 +0530

    Improvement on Task file by coder3

--- Task.py
+++ Task.py
@@ -4,7 +4,7 @@ even_num = (x for x in list1 if x%2 ==0)
 for num in even_num:
     print(num,end=" ")
 
-items = ["apple", "banana", "cherry"]
+items = ["apple", "banana", "cherry","grapes"]
 prices = [0.5, 0.3, 0.2]
 my_dict = {key:value for key,value in zip(items,prices)}
 print(my_dict)
